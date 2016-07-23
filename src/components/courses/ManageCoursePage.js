46d72aa11e2d0f3493d1f8e1209c33d94e39d42e
compumike08 <compumike08@users.noreply.github.com>
2016-07-23 18:12:22 -0400

    Add toastr notification when course saves successfully

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -43,6 +44,7 @@ class ManageCoursePage extends React.Component {
   }
 
   redirect(){
+    toastr.success('Course saved');
     this.context.router.push('/courses');
   }
 
