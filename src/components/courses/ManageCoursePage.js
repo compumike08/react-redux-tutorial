51f93392a399662cfa5d0d6d02c459cc95c34b91
compumike08 <compumike08@users.noreply.github.com>
2016-07-23 18:08:11 -0400

    Change 'Save' button on ManageCoursePage to indicate when saving is in progress

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -10,7 +10,8 @@ class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
 
     this.updateCourseState = this.updateCourseState.bind(this);
@@ -33,8 +34,12 @@ class ManageCoursePage extends React.Component {
 
   saveCourse(event) {
     event.preventDefault();
+    this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
-      .then(() => this.redirect());
+      .then(() => {
+        this.setState({saving: false});
+        this.redirect();
+      });
   }
 
   redirect(){
@@ -49,6 +54,7 @@ class ManageCoursePage extends React.Component {
         onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
       />
     );
   }
