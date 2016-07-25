5c4900c9fae72885f2b2c21b91bd672781da33e7
compumike08 <compumike08@users.noreply.github.com>
2016-07-25 01:22:06 -0400

    Add CREATE_AUTHOR_SUCCESS action and reducer

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,9 +1,13 @@
 import * as types from './actionTypes';
 import AuthorApi from '../api/mockAuthorApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadAuthorsSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+export function createAuthorSuccess(author) {
+  return {type: types.CREATE_AUTHOR_SUCCESS, author};
 }
 
 export function loadAuthors() {
@@ -12,6 +16,22 @@ export function loadAuthors() {
     return AuthorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveAuthor(author) {
+  return function (dispatch, getState) {
+    dispatch(beginAjaxCall());
+    return AuthorApi.saveAuthor(author).then(() => {
+      if (author.id) {
+        //handle dispatching UPDATE_AUTHOR_SUCCESS
+      } else {
+        dispatch(createAuthorSuccess(author));
+      }
+    }).catch(error => {
+      dispatch(ajaxCallError(error));
       throw(error);
     });
   };
