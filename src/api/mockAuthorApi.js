868c8dd1411f3f3e0185082e73fd7f165543a367
compumike08 <compumike08@users.noreply.github.com>
2016-07-24 22:42:24 -0400

    Refactor mockAuthorApi.js to provide more realistic behavior

--- src/api/mockAuthorApi.js
+++ src/api/mockAuthorApi.js
@@ -3,23 +3,75 @@ import delay from './delay';
 // This file mocks a web API by working with the hard-coded data below.
 // It uses setTimeout to simulate the delay of an AJAX call.
 // All calls return promises.
-const authors = [
-  {
-    id: 'cory-house',
-    firstName: 'Cory',
-    lastName: 'House'
-  },
-  {
-    id: 'scott-allen',
-    firstName: 'Scott',
-    lastName: 'Allen'
-  },
-  {
-    id: 'dan-wahlin',
-    firstName: 'Dan',
-    lastName: 'Wahlin'
-  }
-];
+let authorsServerStore = (function(){
+  let authors = [
+    {
+      id: 'cory-house',
+      firstName: 'Cory',
+      lastName: 'House'
+    },
+    {
+      id: 'scott-allen',
+      firstName: 'Scott',
+      lastName: 'Allen'
+    },
+    {
+      id: 'dan-wahlin',
+      firstName: 'Dan',
+      lastName: 'Wahlin'
+    }
+  ];
+
+  return {
+    getAllData: function(){
+      return authors;
+    },
+    saveNewData: function(author){
+      try {
+        authors.push(author);
+      }catch(err){
+        throw new Error("Error saving new author on server");
+      }
+    },
+    updateDataByIndex: function(index, author){
+      try{
+        authors[index] = author;
+      }catch(err){
+        throw new Error("Error updating author on server");
+      }
+    },
+    deleteDataByIndex: function(index){
+      try{
+        authors.splice(index, 1);
+      }catch(err){
+        throw new Error("Error deleting author on server");
+      }
+    },
+    deleteDataById: function(in_id){
+      let authorsAryLen = authors.length;
+      let isFound = null;
+
+      for(let i = 0; i < authorsAryLen; i++){
+        if(authors[i].id == in_id){
+          isFound = i;
+          break;
+        }
+      }
+
+      if(isFound === null){
+        throw new Error("Unable to find author to delete on server");
+      }else{
+        try{
+          authors.splice(isFound, 1);
+        }catch(err){
+          throw new Error("Error deleting author on server");
+        }
+      }
+    }
+  };
+})();
+
+
 
 //This would be performed on the server in a real app. Just stubbing in.
 const generateId = (author) => {
@@ -30,7 +82,7 @@ class AuthorApi {
   static getAllAuthors() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(Object.assign([], authors));
+        resolve(Object.assign([], authorsServerStore.getAllData()));
       }, delay);
     });
   }
@@ -49,14 +101,23 @@ class AuthorApi {
         }
 
         if (author.id) {
-          const existingAuthorIndex = authors.findIndex(a => a.id == author.id);
-          authors.splice(existingAuthorIndex, 1, author);
+          let currentData = authorsServerStore.getAllData();
+          const existingAuthorIndex = currentData.findIndex(a => a.id == author.id);
+          try {
+            authorsServerStore.updateDataByIndex(existingAuthorIndex, author);
+          }catch(err){
+            reject(err.message);
+          }
         } else {
           //Just simulating creation here.
           //The server would generate ids for new authors in a real app.
           //Cloning so copy returned is passed by value rather than by reference.
           author.id = generateId(author);
-          authors.push(author);
+          try {
+            authorsServerStore.saveNewData(author);
+          }catch(err){
+            reject(err.message);
+          }
         }
 
         resolve(Object.assign({}, author));
@@ -67,10 +128,11 @@ class AuthorApi {
   static deleteAuthor(authorId) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        const indexOfAuthorToDelete = authors.findIndex(author => {
-          author.authorId == authorId;
-        });
-        authors.splice(indexOfAuthorToDelete, 1);
+        try{
+          authorsServerStore.deleteDataById(authorId);
+        }catch(err){
+          reject(err.message);
+        }
         resolve();
       }, delay);
     });
