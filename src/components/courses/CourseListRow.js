1cb5b7505e69c98d29f869d9624e02e040a50189
compumike08 <compumike08@users.noreply.github.com>
2016-07-25 00:52:19 -0400

    Make delete button in CourseListRow size btn-sm and & custom CSS class

--- src/components/courses/CourseListRow.js
+++ src/components/courses/CourseListRow.js
@@ -6,7 +6,11 @@ const CourseListRow = ({course, onDelete}) => {
   return (
     <tr>
       <td><a href={course.watchHref} target="_blank">Watch</a></td>
-      <td><button className="btn btn-danger" onClick={onDelete} value={course.id}><FontAwesome name="close"/></button></td>
+      <td>
+        <button className="myapp-button-controls btn btn-danger btn-sm" onClick={onDelete} value={course.id}>
+          <FontAwesome name="close"/>
+        </button>
+      </td>
       <td><Link to={'/course/' + course.id}>{course.title}</Link></td>
       <td>{course.authorId}</td>
       <td>{course.category}</td>
