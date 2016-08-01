2fe525a96bfce9a2c855d148d933aba98136df17
compumike08 <compumike08@users.noreply.github.com>
2016-07-31 23:42:11 -0400

    Add DELETE_AUTHOR_IN_PROGRESS & DELETE_AUTHOR_COMPLETE actions & reducers

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -18,6 +18,14 @@ export function deleteAuthorSuccess(authorId) {
   return {type: types.DELETE_AUTHOR_SUCCESS, authorId};
 }
 
+export function deleteAuthorInProgress(authorId) {
+  return {type: types.DELETE_AUTHOR_IN_PROGRESS, authorId};
+}
+
+export function deleteAuthorComplete(authorId) {
+  return {type: types.DELETE_AUTHOR_COMPLETE, authorId};
+}
+
 export function loadAuthors() {
   return dispatch => {
     dispatch(beginAjaxCall());
@@ -29,10 +37,28 @@ export function loadAuthors() {
   };
 }
 
+export function beginDeleteAuthor(authorId) {
+  return function (dispatch, getState) {
+    dispatch(deleteAuthorInProgress(authorId));
+  };
+}
+
+export function completeDeleteAuthor(authorId) {
+  return function (dispatch, getState) {
+    dispatch(deleteAuthorComplete(authorId));
+  };
+}
+
 export function saveAuthor(author) {
   return function (dispatch, getState) {
+    const inAuthor = {
+      id: author.id,
+      lastName: author.lastName,
+      firstName: author.firstName
+    };
+
     dispatch(beginAjaxCall());
-    return AuthorApi.saveAuthor(author).then(savedAuthor => {
+    return AuthorApi.saveAuthor(inAuthor).then(savedAuthor => {
       if (author.id) {
         dispatch(updateAuthorSuccess(savedAuthor));
       } else {
@@ -47,6 +73,7 @@ export function saveAuthor(author) {
 
 export function deleteAuthor(authorId) {
   return function (dispatch, getState) {
+    dispatch(beginDeleteAuthor(authorId));
     const currentState = getState();
 
     let coursesWithAuthor = currentState.courses.filter(course => course.authorId === authorId);
@@ -55,13 +82,16 @@ export function deleteAuthor(authorId) {
       dispatch(beginAjaxCall());
       return AuthorApi.deleteAuthor(authorId).then(() => {
         dispatch(deleteAuthorSuccess(authorId));
+        dispatch(completeDeleteAuthor(authorId));
       }).catch(error => {
         dispatch(ajaxCallError(error));
+        dispatch(completeDeleteAuthor(authorId));
         throw(error);
       });
     } else {
       const errMessage = "Cannot delete author who is attached to one or more courses";
       return new Promise((resolve, reject) => {
+        dispatch(completeDeleteAuthor(authorId));
         reject(errMessage);
       });
     }
--- src/reducers/authorReducer.js
+++ src/reducers/authorReducer.js
@@ -2,28 +2,49 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function authorReducer(state = initialState.authors, action) {
-  switch (action.type) {
-    case types.LOAD_AUTHORS_SUCCESS:
-      return action.authors;
+  if (action.type === types.LOAD_AUTHORS_SUCCESS) {
+    const aryLen = action.authors.length;
 
-    case types.CREATE_AUTHOR_SUCCESS:
+    for(let i = 0; i < aryLen; i++){
+      action.authors[i].deleting = false;
+    }
+
+    return action.authors;
+  } else if (action.type === types.CREATE_AUTHOR_SUCCESS) {
+    return [
+      ...state,
+      Object.assign({}, action.author)
+    ];
+  } else if (action.type === types.UPDATE_AUTHOR_SUCCESS) {
+    return [
+      ...state.filter(author => author.id !== action.author.id),
+      Object.assign({}, action.author)
+    ];
+  } else if (action.type === types.DELETE_AUTHOR_SUCCESS) {
+    return [
+      ...state.filter(author => author.id !== action.authorId)
+    ];
+  } else if (action.type === types.DELETE_AUTHOR_IN_PROGRESS) {
+    let authorAry = [...state.filter(author => author.id === action.authorId)];
+    let authorObj = Object.assign({}, authorAry[0]);
+    authorObj.deleting = true;
+    return [
+      ...state.filter(author => author.id !== action.authorId),
+      Object.assign({}, authorObj)
+    ];
+  } else if (action.type === types.DELETE_AUTHOR_COMPLETE) {
+    let authorAry = [...state.filter(author => author.id === action.authorId)];
+    let authorObj = Object.assign({}, authorAry[0]);
+    if (authorObj && authorObj.hasOwnProperty("id") && authorObj.id) {
+      authorObj.deleting = false;
       return [
-        ...state,
-        Object.assign({}, action.author)
+        ...state.filter(author => author.id !== action.authorId),
+        Object.assign({}, authorObj)
       ];
-
-    case types.UPDATE_AUTHOR_SUCCESS:
-      return [
-        ...state.filter(author => author.id !== action.author.id),
-        Object.assign({}, action.author)
-      ];
-
-    case types.DELETE_AUTHOR_SUCCESS:
-      return [
-        ...state.filter(author => author.id !== action.authorId)
-      ];
-
-    default:
+    } else {
       return state;
+    }
+  } else {
+    return state;
   }
 }
