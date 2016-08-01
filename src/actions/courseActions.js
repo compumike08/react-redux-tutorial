4c35fb1515fcda7761159cebc3b253d97f5caccb
compumike08 <compumike08@users.noreply.github.com>
2016-08-01 00:53:58 -0400

    Add DELETE_COURSE_IN_PROGRESS & DELETE_COURSE_COMPLETE actions & reducers

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -18,6 +18,14 @@ export function deleteCourseSuccess(courseId) {
   return {type: types.DELETE_COURSE_SUCCESS, courseId: courseId};
 }
 
+export function deleteCourseInProgress(courseId) {
+  return {type: types.DELETE_COURSE_IN_PROGRESS, courseId: courseId};
+}
+
+export function deleteCourseComplete(courseId) {
+  return {type: types.DELETE_COURSE_COMPLETE, courseId: courseId};
+}
+
 
 export function loadCourses(){
   return function(dispatch){
@@ -27,6 +35,18 @@ export function loadCourses(){
     }).catch(error => {
       throw(error);
     });
+  };
+}
+
+export function beginDeleteCourse(courseId) {
+  return function (dispatch, getState) {
+    dispatch(deleteCourseInProgress(courseId));
+  };
+}
+
+export function completeDeleteCourse(courseId) {
+  return function (dispatch, getState) {
+    dispatch(deleteCourseComplete(courseId));
   };
 }
 
@@ -44,11 +64,14 @@ export function saveCourse(course) {
 
 export function deleteCourse(courseId) {
   return function (dispatch, getState) {
+    dispatch(beginDeleteCourse(courseId));
     dispatch(beginAjaxCall());
     return courseApi.deleteCourse(courseId).then(() => {
       dispatch(deleteCourseSuccess(courseId));
+      dispatch(completeDeleteCourse(courseId));
     }).catch(error => {
       dispatch(ajaxCallError(error));
+      dispatch(completeDeleteCourse(courseId));
       throw(error);
     });
 
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,28 +2,51 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
-  switch (action.type) {
-    case types.LOAD_COURSES_SUCCESS:
-      return action.courses;
+  if (action.type === types.LOAD_COURSES_SUCCESS) {
+    const aryLen = action.courses.length;
 
-    case types.CREATE_COURSE_SUCCESS:
+    for (let i = 0; i < aryLen; i++) {
+      action.courses[i].deleting = false;
+    }
+
+    return action.courses;
+  } else if (action.type === types.CREATE_COURSE_SUCCESS) {
+    return [
+      ...state,
+      Object.assign({}, action.course)
+    ];
+  } else if (action.type === types.UPDATE_COURSE_SUCCESS) {
+    return [
+      ...state.filter(course => course.id !== action.course.id),
+      Object.assign({}, action.course)
+    ];
+  } else if (action.type === types.DELETE_COURSE_SUCCESS) {
+    return [
+      ...state.filter(course => course.id !== action.courseId)
+    ];
+  } else if (action.type === types.DELETE_COURSE_IN_PROGRESS) {
+    let courseAry = state.filter(course => course.id === action.courseId);
+    let courseObj = Object.assign({}, courseAry[0]);
+    courseObj.deleting = true;
+
+    return [
+      ...state.filter(course => course.id !== action.courseId),
+      Object.assign({}, courseObj)
+    ];
+  } else if (action.type === types.DELETE_COURSE_COMPLETE) {
+    let courseAry = state.filter(course => course.id === action.courseId);
+    let courseObj = Object.assign({}, courseAry[0]);
+
+    if (courseObj && courseObj.hasOwnProperty("id") && courseObj.id) {
+      courseObj.deleting = false;
       return [
-        ...state,
-        Object.assign({}, action.course)
+        ...state.filter(course => course.id !== action.courseId),
+        Object.assign({}, courseObj)
       ];
-
-    case types.UPDATE_COURSE_SUCCESS:
-      return [
-        ...state.filter(course => course.id !== action.course.id),
-        Object.assign({}, action.course)
-      ];
-
-    case types.DELETE_COURSE_SUCCESS:
-      return [
-        ...state.filter(course => course.id !== action.courseId)
-      ];
-
-    default:
+    } else {
       return state;
+    }
+  } else {
+    return state;
   }
 }
